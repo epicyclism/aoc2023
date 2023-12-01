2a6b2a3ea0e9b2b63ffeacd5c2644392e11c115e
Paul  Ranson <paulranson@Pauls-MacBook-Air.local>
2023-12-01 09:53:07 +0000

    aoc1 pt 1

--- aoc1/aoc1.cpp
+++ aoc1/aoc1.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <numeric>
+
+auto get_input()
+{
+	std::vector<std::string> vin;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+		vin.emplace_back(ln);
+	return vin;
+}
+
+int pt1(auto const& in)
+{
+	return std::accumulate(in.begin(), in.end(), 0, [](auto l, auto& r)
+		{
+			int ld { r[r.find_first_of("0123456789")] - '0'};
+			int rd { r[r.find_last_of("0123456789")] - '0'};
+//			std::cout << r << " " << ld << ", " << rd << "\n";
+			return l + ld * 10 + rd;
+		});
+}
 
 int main()
 {
-	std::cout << "Hello World\n";
+	auto in { get_input()};
+	std::cout << "got " << in.size() << " input strings.\n";
+	std::cout << "pt1 = " << pt1(in) << "\n";
 }
