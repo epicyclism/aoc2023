0da6cda48c85bb8599630ca31a52081cc9df3652
epicyclism <paul@epicyclism.com>
2023-12-06 11:18:11 +0000

    aoc6

--- aoc6/aoc6.cpp
+++ aoc6/aoc6.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <numeric>
+
+#include "ctre_inc.h"
+
+auto get_input()
+{
+	std::vector<std::pair<int, int>> rv;
+	std::string ln;
+	std::getline(std::cin, ln);
+	for (auto m : ctre::search_all<"(\\d+)">(ln))
+		rv.emplace_back(m.to_number<int>(), 0);
+	std::getline(std::cin, ln);
+	auto i{ rv.begin() };
+	for (auto m : ctre::search_all<"(\\d+)">(ln))
+		(*i++).second = m.to_number<int>();
+	return rv;
+}
+
+auto count_above(auto tm, auto abv)
+{
+	decltype(abv) rv{ 0 };
+	for (auto t = 0; t < tm; ++t)
+	{
+		auto d = t * (tm - t);
+		if (d > abv)
+			++rv;
+	}
+	return rv;
+}
+
+auto pt1(auto const& in)
+{
+	int rv{ 1 };
+	for (auto& r : in)
+		rv *= count_above(r.first, r.second);
+	return rv;
+}
+
+int factor(int v)
+{
+	if (v < 10)
+		return 10;
+	if (v < 100)
+		return 100;
+	if (v < 1000)
+		return 1000;
+	if (v < 10000)
+		return 10000;
+	return 100000; // tilt
+}
+
+auto pt2(auto const& in)
+{
+	long long tm{ 0 };
+	long long d{ 0 };
+	for (auto& td : in)
+	{
+		tm *= factor(td.first);
+		tm += td.first;
+		d *= factor(td.second);
+		d += td.second;
+	}
+	return count_above(tm, d);
+}
 
 int main()
 {
-	std::cout << "Hello World\n";
+	auto in{ get_input() };
+	std::cout << "pt1 = " << pt1(in) << "\n";
+	std::cout << "pt2 = " << pt2(in) << "\n";
 }
